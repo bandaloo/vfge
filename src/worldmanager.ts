765bbf9779e514b0eb164f87f1d5ad30c2d28ec2
bandaloo <bandaloo@icloud.com>
2020-01-23 18:45:53 -0500

    working on world manager implementation

--- src/worldmanager.ts
+++ src/worldmanager.ts
@@ -1,42 +1,83 @@
 import { Entity } from "./entity";
 import { inPlaceFilter } from "./helpers";
 
-const DELTA_TIME_CAP = 200;
+export namespace WorldManager {
+  const DELTA_TIME_CAP = 200;
 
-const updateTime = 1000 / 240; // Run game logic (not drawing) at 240 FPS.
-let previousTime = 0;
-let overTime = 0;
-let totalTime = 0;
+  const updateTime = 1000 / 240; // Run game logic (not drawing) at 240 FPS.
+  let previousTime = 0;
+  let overTime = 0;
+  let totalTime = 0;
 
-/**
- * Set the drawing position based on the amount of time left in a frame.
- * @param entities list of entities to tween
- * @param timeLeft time used for tweening
- */
-function performTween(entities: Entity[], timeLeft: number) {
-  for (const entity of entities) {
-    entity.drawPos = entity.lastPos.partway(
-      entity.pos,
-      (updateTime + timeLeft) / updateTime
+  /**
+   * Update the last position in order to prepare for the tweening step.
+   * @param entities list of entities to update last position of
+   */
+  function prepareTween(entities: Entity[]) {
+    for (let i = 0; i < entities.length; i++) {
+      entities[i].lastPos = entities[i].pos;
+    }
+  }
+
+  /**
+   * Set the drawing position based on the amount of time left in a frame.
+   * @param entities list of entities to tween
+   * @param timeLeft time used for tweening
+   */
+  function performTween(entities: Entity[], timeLeft: number) {
+    for (const entity of entities) {
+      entity.drawPos = entity.lastPos.partway(
+        entity.pos,
+        (updateTime + timeLeft) / updateTime
+      );
+    }
+  }
+
+  function gameStepEntities(entities: Entity[]) {
+    for (const entity of entities) {
+      entity.action();
+    }
+  }
+
+  function physicsStepEntities(entities: Entity[]) {
+    for (const entity of entities) {
+      entity.step();
+    }
+  }
+
+  function ageStepEntities(entities: Entity[]) {
+    for (const entity of entities) {
+      entity.lifetime--;
+      if (entity.lifetime <= 0) {
+        entity.deleteMe = true;
+      }
+    }
+  }
+
+  /**
+   * Destroys every entity that needs to be deleted.
+   * @param entities entity list to filter from
+   */
+  function destroyEntities(entities: Entity[]) {
+    inPlaceFilter<Entity>(
+      entities,
+      entity => entity.lifetime > 0 && !entity.deleteMe,
+      entity => entity.destroy()
     );
   }
-}
 
-/**
- * Destroys every entity that needs to be deleted.
- * @param entities entity list to filter from
- */
-function destroyEntities(entities: Entity[]) {
-  inPlaceFilter<Entity>(
-    entities,
-    entity => entity.lifetime > 0 && !entity.deleteMe,
-    entity => entity.destroy()
-  );
-}
+  const gameEntities = new Array<Entity>();
 
-export namespace WorldManager {
-  // TODO think about whether this should be exposed
-  const entities = new Array<Entity>();
+  function stepGame() {
+    // Let all entities take their actions.
+    gameStepEntities(gameEntities);
+    // Run a physics step on the world.
+    physicsStepEntities(gameEntities);
+    // Age all the entities.
+    ageStepEntities(gameEntities);
+    // Delete the entities that should be deleted.
+    destroyEntities(gameEntities);
+  }
 
   export function update(currentTime = updateTime) {
     let deltaTime = currentTime - previousTime;
@@ -51,15 +92,15 @@ export namespace WorldManager {
     // Run multiple game update steps based on the amount of time left.
     while (timeLeft > 0) {
       if (timeLeft <= this.updateTime) {
-        // TODO prepare tween on entities
+        prepareTween(gameEntities);
       }
-      // TODO step the game.
+      stepGame();
       timeLeft -= updateTime;
       // Reaching this line means one game step has passed.
     }
 
     // Set all the tweened vectors to the draw positions.
-    performTween(entities, timeLeft);
+    performTween(gameEntities, timeLeft);
     overTime = -timeLeft;
 
     // TODO draw game
